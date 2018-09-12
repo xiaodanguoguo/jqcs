720cdf2885bbbe9ddebedc5030ed92529accca59
lrz0828 <16601106677@163.com>
2018-09-12 12:50:54 +0800

    修改配置文件 修改启动脚本

--- jq/jq-services/base/base-facade/src/main/assembly/bin/start.sh
+++ jq/jq-services/base/base-facade/src/main/assembly/bin/start.sh
@@ -7,7 +7,7 @@ APP_NAME=base-facade-0.0.1-SNAPSHOT
 APP_DIR="${DEPLOY_DIR}/lib"
 rm -f tpid
 
-nohup java -Dloader.path=$DEPLOY_DIR/lib -jar $APP_DIR/$APP_NAME.jar  --spring.config.location=classpath:props/local/def.properties  > $BIN_DIR/run.log  2>&1 &
+nohup java -Dloader.path=$DEPLOY_DIR/lib -jar $APP_DIR/$APP_NAME.jar  --spring.config.location=classpath:props/$1/def.properties  > $BIN_DIR/run.log  2>&1 &
 
 echo $! > tpid
 
--- jq/jq-services/cust/cust-facade/src/main/assembly/bin/check.sh
+++ jq/jq-services/cust/cust-facade/src/main/assembly/bin/check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-APP_NAME=base-facade-0.0.1-SNAPSHOT
+APP_NAME=cust-facade-0.0.1-SNAPSHOT
 
 tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
 if [ ${tpid} ]; then
