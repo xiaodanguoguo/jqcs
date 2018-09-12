ef1178706bf34b1045f63d4a0c9bf325ebd8d74c
mengLee1993 <17611639379@163.com>
2018-09-12 16:01:17 +0800

    【酒钢】

--- jq/jq-webapps/webapps-cs/src/main/resources/static/project/appealCompensate/js/objectionSubmit.js
+++ jq/jq-webapps/webapps-cs/src/main/resources/static/project/appealCompensate/js/objectionSubmit.js
@@ -37,7 +37,7 @@ function clsMethodLee$init(){
 }
 function clsMethodLee$parse(){
     $("#tableList")[0].cacheArr = [];
-    initplugPath($("#condproductCategoryCode")[0],"singleSelectCtrl",this.requestUrl.path7,null,"POST");
+    initplugPath($("#condprodectId")[0],"singleSelectCtrl",this.requestUrl.path7,null,"POST");
     initplugPath($("#tableList")[0],"standardTableCtrl",this.requestUrl.path1,null,"POST");
     // 初始化搜索框下拉
     $("#condclaimState").chosen({
