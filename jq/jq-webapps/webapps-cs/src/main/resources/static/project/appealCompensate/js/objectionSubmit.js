6d545627cdcfc9b1becace71716ec7dd88c7b5ca
mengLee1993 <17611639379@163.com>
2018-09-16 20:12:46 +0800

    【酒钢】

--- jq/jq-webapps/webapps-cs/src/main/resources/static/project/appealCompensate/js/objectionSubmit.js
+++ jq/jq-webapps/webapps-cs/src/main/resources/static/project/appealCompensate/js/objectionSubmit.js
@@ -37,7 +37,7 @@ function clsMethodLee$init(){
 }
 function clsMethodLee$parse(){
     $("#tableList")[0].cacheArr = [];
-    initplugPath($("#condsid")[0],"singleSelectCtrl",this.requestUrl.path7,null,"POST");
+    initplugPath($("#condsid")[0],"singleSelectCtrl",this.requestUrl.path7,{"typeId": "MILL_BIG_TYPE"},"POST");
     initplugPath($("#tableList")[0],"standardTableCtrl",this.requestUrl.path1,null,"POST");
     // 初始化搜索框下拉
     $("#condclaimState").chosen({
