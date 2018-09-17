2988f89e6d867c76204a89d4f9e0e76b4bcde9d7
mengLee1993 <17611639379@163.com>
2018-09-17 11:13:35 +0800

    【酒钢】

--- jq/jq-webapps/webapps-cs/src/main/resources/static/project/login/js/mlogin.js
+++ jq/jq-webapps/webapps-cs/src/main/resources/static/project/login/js/mlogin.js
@@ -174,7 +174,11 @@ function submitCallBack(data){
             setCookie("acctTitle",$("#js-input--username").val());
             setCookie("acctPassword",$("#js-input-password").val());
         }
-        jumpUrl("../../homePage/html-gulp-www/homePage.html","0000000",0);
+        if(GetQueryString("overtime") == 1){
+            jumpUrl(unescape(getCookie("urlPath")),"0000000",0);
+        }else{
+            jumpUrl("../../homePage/html-gulp-www/homePage.html","0000000",0);
+        }
     }/*else {
         var alertBox=new clsAlertBoxCtrl();
         alertBox.Alert(data.retDesc,"失败提示",1,"","loginTipErrTip");
