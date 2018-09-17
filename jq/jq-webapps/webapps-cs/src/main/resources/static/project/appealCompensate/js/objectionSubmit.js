e42fff150648767fbac41b40bd29718aa39d5082
mengLee1993 <17611639379@163.com>
2018-09-17 18:01:21 +0800

    【酒钢】

--- jq/jq-webapps/webapps-cs/src/main/resources/static/project/appealCompensate/js/objectionSubmit.js
+++ jq/jq-webapps/webapps-cs/src/main/resources/static/project/appealCompensate/js/objectionSubmit.js
@@ -276,8 +276,7 @@ function clsStandardTableCtrl$progress(jsonItem, cloneRow) {
         });
         //下载协议书
         $(cloneRow).find("#downloadOpe").on("click",function(){
-            var importParam = "name=" + jsonItem.claimNo;
-            $.download(requestUrl + document.body.jsLee.requestUrl.path8, importParam, "POST");
+           getAjaxResult(document.body.jsLee.requestUrl.path1,"POST",{"templateType":1,"claimNo":jsonItem.claimNo},"pdfViewCallBack(data)");
         });
     }
 }
@@ -396,6 +395,13 @@ function clsAlertBoxCtrl$sure() {
         closePopupWin();
         getAjaxResult(document.body.jsLee.requestUrl.path2,"POST",document.body.jsLee.claimNoArr,"deleteOpeCallBack(data)");
 
+    }
+}
+
+function pdfViewCallBack(data){
+    data = JSON.parse(data);
+    if(data.retCode == "0000000"){
+        jumpUrl("../../appealCompensate/html-gulp-www/pdfView.html")
     }
 }
 
