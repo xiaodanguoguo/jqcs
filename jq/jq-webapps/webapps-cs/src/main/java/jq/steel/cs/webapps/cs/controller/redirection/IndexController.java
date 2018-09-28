83fc09fe56bf0bc6fa03e50c045b674fc4f3ec1d
lrz0828 <16601106677@163.com>
2018-09-28 11:07:32 +0800

    配置文件修改

--- jq/jq-webapps/webapps-cs/src/main/java/jq/steel/cs/webapps/cs/controller/redirection/IndexController.java
+++ jq/jq-webapps/webapps-cs/src/main/java/jq/steel/cs/webapps/cs/controller/redirection/IndexController.java
@@ -1,5 +1,10 @@
 package jq.steel.cs.webapps.cs.controller.redirection;
 
+import com.ebase.core.log.SearchableLoggerFactory;
+import jq.steel.cs.services.base.api.controller.AcctAPI;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,9 +20,30 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class IndexController {
 
+    private final static Logger logger = SearchableLoggerFactory.getDefaultLogger();
+
+    @Autowired
+    private AcctAPI acctAPI;
+
+    @Value("${acct.register.url}")
+    private String url;
+
     @RequestMapping({"/", "/index", ""})
     public String redirection(HttpServletRequest request, HttpServletResponse response) {
 
-        return "login";
+        //验证用户是否登录..
+//        String sessionId = CookieUtil.getSessionId(request);
+//
+//        ServiceResponse<AcctSession> serviceResponse =  acctAPI.getUser(sessionId);
+//
+//        if (!ServiceResponse.SUCCESS_CODE.equals(serviceResponse.getRetCode()) || serviceResponse.getRetContent() == null) {
+//            //获得缓存失败或登录状态无效 调到登录页面
+//            logger.info("登录状态无效!");
+            return "login";
+
+
+//        } else {
+//            return url + "/project/homePage/html-gulp-www/homePage";
+//        }
     }
 }
