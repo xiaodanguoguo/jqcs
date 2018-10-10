6a3eeba4a7f3f02c43b3d4ad0bef185c4c424312
careybin <careybin@gmail.com>
2018-10-10 19:59:36 +0800

    下载预览

--- jq/jq-webapps/webapps-cs/src/main/java/jq/steel/cs/webapps/cs/controller/objection/ObjectionChuLiController.java
+++ jq/jq-webapps/webapps-cs/src/main/java/jq/steel/cs/webapps/cs/controller/objection/ObjectionChuLiController.java
@@ -237,18 +237,15 @@ public class ObjectionChuLiController {
                 //report = uploadConfig.getDomain() +"/"+serviceResponse.getRetContent().getReport();
                 String  pdfName = jsonRequest.getReqBody().getClaimNo() + "S.pdf";
                 String report1 = createPdf.createPdf(jsonRequest.getReqBody().getClaimNo() ,createPdfPath,pdfName,"shoulidan");
-                String hh1 = report1.replace("/data/upload","/res");
-                report =uploadConfig.getDomain()+hh1;
+                report =uploadConfig.getDomain()+pathPattern + uploadConfig.getReportUrl() + report1;
             }else if(jsonRequest.getReqBody().getTemplateType()==6){
                 String  pdfName = jsonRequest.getReqBody().getClaimNo() + "X.pdf";
                 String report1 = createPdf.createPdf(jsonRequest.getReqBody().getClaimNo() ,createPdfPath,pdfName,"xieyishu");
-                String hh1 = report1.replace("/data/upload","/res");
-                report =uploadConfig.getDomain()+hh1;
+                report =uploadConfig.getDomain()+pathPattern + uploadConfig.getReportUrl() + report1;
             }else if(jsonRequest.getReqBody().getTemplateType()==7){
                 String  pdfName = jsonRequest.getReqBody().getClaimNo() + "T.pdf";
                 String report1 = createPdf.createPdf(jsonRequest.getReqBody().getClaimNo() ,createPdfPath,pdfName,"tongzhidan");
-                String hh1 = report1.replace("/data/upload","/res");
-                report =uploadConfig.getDomain()+hh1;
+                report =uploadConfig.getDomain()+pathPattern + uploadConfig.getReportUrl() + report1;
             }
             serviceResponse.getRetContent().setReport(report);
             jsonResponse.setRspBody(serviceResponse.getRetContent());
