9f9504f5f6aa658e41195ce4b2035ede7cc3ec21
careybin <careybin@gmail.com>
2018-10-16 11:12:11 +0800

    調試

--- jq/jq-services/cust/cust-api/src/main/java/jq/steel/cs/services/cust/api/vo/ObjectionLedgerVO.java
+++ jq/jq-services/cust/cust-api/src/main/java/jq/steel/cs/services/cust/api/vo/ObjectionLedgerVO.java
@@ -1,8 +1,279 @@
 package jq.steel.cs.services.cust.api.vo;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 //异议统计台账
 public class ObjectionLedgerVO {
 
-    
+    private String claimNo;
+
+    private String lastUser;
+
+    private String lastUserAddr;
+
+    private String createEmpNo;
+
+    private String lastUserTel;
+
+    private String customerName;
+
+    private String custEmpNo;
+
+    private String custTel;
+
+    private String endProcessingTech;
+
+    private String used;
+
+    private BigDecimal originalWeight;
+
+    private String millSheetNo;
+
+    private String battenPlateNo;
+
+    private String designation;
+
+    private BigDecimal objectionNum;
+
+    private String claimReason;
+
+    private String proProblem;
+
+    private String claimType;
+
+    private String logisticsProcess;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date productDt;
+
+    private String handingSuggestion;
+
+    //异议确认量
+    private  String objectionConfirmation;
+
+    //受理时间
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date admissibilityTime;
+
+    //结案时间
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date closingTime;
+
+    //结案人
+    private String closingUser;
+
+    //备注
+    private String memo;
+
+    public String getClaimNo() {
+        return claimNo;
+    }
+
+    public void setClaimNo(String claimNo) {
+        this.claimNo = claimNo;
+    }
+
+    public String getLastUser() {
+        return lastUser;
+    }
+
+    public void setLastUser(String lastUser) {
+        this.lastUser = lastUser;
+    }
+
+    public String getLastUserAddr() {
+        return lastUserAddr;
+    }
+
+    public void setLastUserAddr(String lastUserAddr) {
+        this.lastUserAddr = lastUserAddr;
+    }
+
+    public String getCreateEmpNo() {
+        return createEmpNo;
+    }
+
+    public void setCreateEmpNo(String createEmpNo) {
+        this.createEmpNo = createEmpNo;
+    }
+
+    public String getLastUserTel() {
+        return lastUserTel;
+    }
+
+    public void setLastUserTel(String lastUserTel) {
+        this.lastUserTel = lastUserTel;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustEmpNo() {
+        return custEmpNo;
+    }
+
+    public void setCustEmpNo(String custEmpNo) {
+        this.custEmpNo = custEmpNo;
+    }
+
+    public String getCustTel() {
+        return custTel;
+    }
+
+    public void setCustTel(String custTel) {
+        this.custTel = custTel;
+    }
+
+    public String getEndProcessingTech() {
+        return endProcessingTech;
+    }
+
+    public void setEndProcessingTech(String endProcessingTech) {
+        this.endProcessingTech = endProcessingTech;
+    }
+
+    public String getUsed() {
+        return used;
+    }
+
+    public void setUsed(String used) {
+        this.used = used;
+    }
+
+    public BigDecimal getOriginalWeight() {
+        return originalWeight;
+    }
+
+    public void setOriginalWeight(BigDecimal originalWeight) {
+        this.originalWeight = originalWeight;
+    }
+
+    public String getMillSheetNo() {
+        return millSheetNo;
+    }
+
+    public void setMillSheetNo(String millSheetNo) {
+        this.millSheetNo = millSheetNo;
+    }
+
+    public String getBattenPlateNo() {
+        return battenPlateNo;
+    }
+
+    public void setBattenPlateNo(String battenPlateNo) {
+        this.battenPlateNo = battenPlateNo;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public BigDecimal getObjectionNum() {
+        return objectionNum;
+    }
+
+    public void setObjectionNum(BigDecimal objectionNum) {
+        this.objectionNum = objectionNum;
+    }
+
+    public String getClaimReason() {
+        return claimReason;
+    }
+
+    public void setClaimReason(String claimReason) {
+        this.claimReason = claimReason;
+    }
+
+    public String getProProblem() {
+        return proProblem;
+    }
+
+    public void setProProblem(String proProblem) {
+        this.proProblem = proProblem;
+    }
+
+    public String getClaimType() {
+        return claimType;
+    }
+
+    public void setClaimType(String claimType) {
+        this.claimType = claimType;
+    }
+
+    public String getLogisticsProcess() {
+        return logisticsProcess;
+    }
+
+    public void setLogisticsProcess(String logisticsProcess) {
+        this.logisticsProcess = logisticsProcess;
+    }
+
+    public Date getProductDt() {
+        return productDt;
+    }
+
+    public void setProductDt(Date productDt) {
+        this.productDt = productDt;
+    }
+
+    public String getHandingSuggestion() {
+        return handingSuggestion;
+    }
+
+    public void setHandingSuggestion(String handingSuggestion) {
+        this.handingSuggestion = handingSuggestion;
+    }
+
+    public String getObjectionConfirmation() {
+        return objectionConfirmation;
+    }
+
+    public void setObjectionConfirmation(String objectionConfirmation) {
+        this.objectionConfirmation = objectionConfirmation;
+    }
+
+    public Date getAdmissibilityTime() {
+        return admissibilityTime;
+    }
+
+    public void setAdmissibilityTime(Date admissibilityTime) {
+        this.admissibilityTime = admissibilityTime;
+    }
+
+    public Date getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(Date closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public String getClosingUser() {
+        return closingUser;
+    }
+
+    public void setClosingUser(String closingUser) {
+        this.closingUser = closingUser;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
 }
