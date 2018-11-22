945ad289ab022a189c8017ef34d8cce436d6f50b
careybin <careybin@gmail.com>
2018-11-22 15:46:13 +0800

    发车时间就显示日期就好，不用显示小时、分钟、秒信息

--- jq/jq-services/cust/cust-api/src/main/java/jq/steel/cs/services/cust/api/vo/MillSheetHostsVO.java
+++ jq/jq-services/cust/cust-api/src/main/java/jq/steel/cs/services/cust/api/vo/MillSheetHostsVO.java
@@ -141,7 +141,7 @@ public class MillSheetHostsVO implements Serializable {
 
     private String createdBy;
 
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
     private Date createdDt;
 
     private String updatedBy;
