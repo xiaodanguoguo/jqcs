b2c68d2c676964b912a874a7424c024bf219e1a4
careybin <careybin@gmail.com>
2019-02-21 10:43:15 +0800

    app 客户评价添加分页

--- jq/jq-services/cust/cust-api/src/main/java/jq/steel/cs/services/cust/api/vo/CrmClaimCommentsVO.java
+++ jq/jq-services/cust/cust-api/src/main/java/jq/steel/cs/services/cust/api/vo/CrmClaimCommentsVO.java
@@ -5,6 +5,26 @@ import java.util.Date;
 
 public class CrmClaimCommentsVO {
 
+    private int pageNum = 1;
+
+    private int pageSize = 10;
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
     private  String orgCode;
 
     private  String orgName;
