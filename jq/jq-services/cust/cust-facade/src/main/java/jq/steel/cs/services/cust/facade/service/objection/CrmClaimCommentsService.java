b40d636fcdc5136a525372eff327b45cb978b1c5
careybin <careybin@gmail.com>
2019-02-21 08:45:40 +0800

    质证书信息 车号模糊和数据权限钢厂查询

--- jq/jq-services/cust/cust-facade/src/main/java/jq/steel/cs/services/cust/facade/service/objection/CrmClaimCommentsService.java
+++ jq/jq-services/cust/cust-facade/src/main/java/jq/steel/cs/services/cust/facade/service/objection/CrmClaimCommentsService.java
@@ -1,9 +1,12 @@
 package jq.steel.cs.services.cust.facade.service.objection;
 
+import com.ebase.core.page.PageDTO;
 import jq.steel.cs.services.cust.api.vo.CrmClaimCommentsVO;
 import jq.steel.cs.services.cust.api.vo.CrmLastuserInfoVO;
 
 public interface CrmClaimCommentsService {
 
     Integer evaluate (CrmClaimCommentsVO crmClaimCommentsVO);
+
+    PageDTO<CrmClaimCommentsVO> findByPage(CrmClaimCommentsVO crmClaimCommentsVO);
 }
