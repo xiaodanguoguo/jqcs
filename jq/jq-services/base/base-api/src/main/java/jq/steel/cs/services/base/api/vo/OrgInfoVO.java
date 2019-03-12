48f6162a9f9d5a4dd0c2a12f9f5a1df4d1b4fc1c
careybin <careybin@gmail.com>
2019-03-12 11:01:05 +0800

    注冊

--- jq/jq-services/base/base-api/src/main/java/jq/steel/cs/services/base/api/vo/OrgInfoVO.java
+++ jq/jq-services/base/base-api/src/main/java/jq/steel/cs/services/base/api/vo/OrgInfoVO.java
@@ -117,6 +117,41 @@ public class OrgInfoVO{
 	// 注册类型
 	private Integer regType;
 
+
+	//购买产品类别
+	private String productClassification;
+
+	//公司所在区域
+	private String area;
+
+	//对应的销售公司
+	private String salesCompany;
+
+
+	public String getProductClassification() {
+		return productClassification;
+	}
+
+	public void setProductClassification(String productClassification) {
+		this.productClassification = productClassification;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getSalesCompany() {
+		return salesCompany;
+	}
+
+	public void setSalesCompany(String salesCompany) {
+		this.salesCompany = salesCompany;
+	}
+
 	public OrgInfoVO getParent() {
 		return parent;
 	}
