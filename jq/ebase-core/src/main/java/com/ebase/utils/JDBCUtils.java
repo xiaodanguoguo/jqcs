a41f159730cd78bb8daa1180352fd1f7777e1b74
careybin <careybin@gmail.com>
2019-05-07 19:21:56 +0800

    //2019-03-27 管理联系信息代理单位查询出错

--- jq/ebase-core/src/main/java/com/ebase/utils/JDBCUtils.java
+++ jq/ebase-core/src/main/java/com/ebase/utils/JDBCUtils.java
@@ -5,10 +5,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
- 
+ //存储过程调用数据库的配置文件。本地启动需注意（操作触发点在拆分质证书提交按钮上，配置出问题可能导致拆分测试数据库/正式数据库数据混乱，质证书生成不出来）
 public class JDBCUtils {
     private static String driver="oracle.jdbc.OracleDriver";
+    //生产
     //private static String url="jdbc:oracle:thin:@10.1.213.125:1521:jgcrmdbp";
+    //测试
     private static String url="jdbc:oracle:thin:@10.1.213.127:1521:jgcrmdbt";
     private static String user="WARRANTY";
     private static String password="WARRANTY";
