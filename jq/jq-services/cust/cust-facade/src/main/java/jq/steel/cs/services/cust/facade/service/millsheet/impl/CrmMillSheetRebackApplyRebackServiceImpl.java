3d52d4a4933010112aa59a2fef14c5cb0d5ad493
lrz0828 <16601106677@163.com>
2019-07-29 20:43:10 +0800

    更改

--- jq/jq-services/cust/cust-facade/src/main/java/jq/steel/cs/services/cust/facade/service/millsheet/impl/CrmMillSheetRebackApplyRebackServiceImpl.java
+++ jq/jq-services/cust/cust-facade/src/main/java/jq/steel/cs/services/cust/facade/service/millsheet/impl/CrmMillSheetRebackApplyRebackServiceImpl.java
@@ -1,10 +1,11 @@
 package jq.steel.cs.services.cust.facade.service.millsheet.impl;
 
-import com.ebase.core.AssertContext;
 import jq.steel.cs.services.cust.facade.dao.CrmMillSheetRebackApplyMapper;
 import jq.steel.cs.services.cust.facade.dao.MillFallbackInfoMapper;
+import jq.steel.cs.services.cust.facade.dao.MillSheetHostsMapper;
 import jq.steel.cs.services.cust.facade.model.CrmMillSheetRebackApply;
 import jq.steel.cs.services.cust.facade.model.MillFallbackInfo;
+import jq.steel.cs.services.cust.facade.model.MillSheetHosts;
 import jq.steel.cs.services.cust.facade.service.millsheet.CrmMillSheetRebackApplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,8 @@ public class CrmMillSheetRebackApplyRebackServiceImpl implements CrmMillSheetReb
 
     @Autowired
     private MillFallbackInfoMapper millFallbackInfoMapper;
+    @Autowired
+    private MillSheetHostsMapper millSheetHostsMapper;
     @Override
     public CrmMillSheetRebackApply applyForRetreat(CrmMillSheetRebackApply crmMillSheetRebackApply) {
 
@@ -27,7 +30,8 @@ public class CrmMillSheetRebackApplyRebackServiceImpl implements CrmMillSheetReb
         MillFallbackInfo millFallbackInfo1 = new MillFallbackInfo();
         millFallbackInfo1.setMillSheetNo(crmMillSheetRebackApply.getMillSheetNo());
         List<MillFallbackInfo> millFallbackInfoList = millFallbackInfoMapper.selectByPrimaryKey(millFallbackInfo1);
-        if (millFallbackInfoList.size()>0){
+        MillSheetHosts millSheetHosts = millSheetHostsMapper.selectByMillSheetNo(crmMillSheetRebackApply.getMillSheetNo());
+        if (millFallbackInfoList.size() > 0 && !(millSheetHosts.getDeptCode().equals("1000"))){
             crmMillSheetRebackApply.setIsReback("Y");
         }else {
             crmMillSheetRebackApply.setIsReback("N");
